2e467ee8a87451adb76f6e8c7f367f8255c783bd
tylermccormack <mccormacktc@g.cofc.edu>
2021-09-01 10:27:09 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Tyler McCormack
+hello.py
+
+Problem: Write a program that prints "Hello, world!"
+
+Certificate of Authenticity
+I certify that this is my original work
+
+"""
+
+def main():
+    print("hello, world!")
+
+main()
